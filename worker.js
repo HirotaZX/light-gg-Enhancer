e7c8805a3ecf05717e07ffd6283d6839a4c3c3a8
HirotaZX <55805734+HirotaZX@users.noreply.github.com>
2024-06-14 20:15:27 +0800

    Worker: Filter items to reduce size

--- worker.js
+++ worker.js
@@ -2,6 +2,7 @@ import { fetch } from 'undici'
 import * as fs from 'node:fs/promises';
 
 const langList = ['en', 'fr', 'es', 'es-mx', 'de', 'it', 'ja', 'pt-br', 'ru', 'pl', 'ko', 'zh-cht', 'zh-chs'];
+const itemFilter = [1, 21, 59]; // [weapon, armor, mods]
 const itemDefineList = {};
 
 const manifestResp = await fetch('https://www.bungie.net/Platform/Destiny2/Manifest/');
@@ -19,14 +20,18 @@ for (const lang of langList) {
 const combinedItemList = {};
 
 for(const key in itemDefineList[langList[0]]) {
-    const combinedItemName = {};
-    for (const lang of langList) {
-        combinedItemName[lang] = itemDefineList[lang][key].displayProperties.name;
+    const item = itemDefineList[langList[0]][key];
+    // filter items, only keep weapon, armor and mods
+    if(item.itemCategoryHashes && item.itemCategoryHashes.some(hash => itemFilter.includes(hash))) {
+        const combinedItemName = {};
+        for (const lang of langList) {
+            combinedItemName[lang] = itemDefineList[lang][key].displayProperties.name;
+        }
+        combinedItemList[key] = combinedItemName;
     }
-    combinedItemList[key] = combinedItemName;
 }
 
 await fs.rm('dist', { recursive: true, force: true });
 await fs.mkdir('dist', { recursive: true });
 await fs.writeFile('dist/item-list.json', JSON.stringify(combinedItemList));
-console.log('combine done');
+console.log('combine done! filtered and combined ' + Object.keys(combinedItemList).length + ' items!');
