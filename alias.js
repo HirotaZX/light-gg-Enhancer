fb83c755951e0460090dcb404e2e6cb046f7c543
HirotaZX <55805734+HirotaZX@users.noreply.github.com>
2024-06-17 06:34:37 +0800

    Worker: Update alias dict

--- alias.js
+++ alias.js
@@ -2,4 +2,14 @@ export default {
     848860060: ['fttc', '4ttc'], // FOURTH TIME'S THE CHARM
     1354429876: ['fttc', '4ttc'], // FOURTH TIME'S THE CHARM
     1380253176: ['High-Explosive Ordinance'], // HIGH-EXPLOSIVE ORDNANCE
+    1844523823: ['Confine Launch'], // CONFINED LAUNCH
+    4049631843: ['OFA'], // ONE FOR ALL
+    859780267: ['OFA'], // ONE FOR ALL
+    372672210: ['vorpal'], // VORPAL WEAPON REFIT
+    1546637391: ['vorpal'], // VORPAL WEAPON
+    3418165135: ['vorpal'], // VORPAL WEAPON
+    3523296417: ['demo'], // DEMOLITIONIST
+    1906147653: ['demo'], // DEMOLITIONIST
+    2675184851: ['incan'], // INCANDESCENT
+    4293542123: ['incan'], // INCANDESCENT
 }
